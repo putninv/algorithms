5444fd7d7d60b2bcabdd5021d86e7990b293c339
putnin.v <putnin.v@gmail.com>
2019-11-07 19:19:55 +0300

    ALG-010 Merge Sorted Array(easy)

--- src/com/company/main/Main.java
+++ src/com/company/main/Main.java
@@ -1,8 +1,6 @@
 package com.company.main;
 
-import com.company.leetcode.*;
 import com.company.leetcode.model.ListNode;
-import com.company.leetcode.model.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -77,14 +75,16 @@ public class Main {
 
 //        System.out.println(AlphabetBoardPath.alphabetBoardPath("zxz"));
 
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(2);
-        root.left.left = new TreeNode(3);
-        root.left.right = new TreeNode(4);
-        root.right.left = new TreeNode(4);
-        root.right.right = new TreeNode(3);
-        System.out.println(SymmetricTree.isSymmetric(root));
+//        TreeNode root = new TreeNode(1);
+//        root.left = new TreeNode(2);
+//        root.right = new TreeNode(2);
+//        root.left.left = new TreeNode(3);
+//        root.left.right = new TreeNode(4);
+//        root.right.left = new TreeNode(4);
+//        root.right.right = new TreeNode(3);
+//        System.out.println(SymmetricTree.isSymmetric(root));
+
+
     }
 
     private static String stringifyArray(int[] arr){
